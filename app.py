09b2be7f285d214c62ed12f8dbb0fa1fc4af9450
Lino Bollansee <linobollansee@hotmail.com>
2024-12-23 08:50:28 +0000

    Load page_project_hypothesis_body in app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from app_pages.multipage import MultiPage
 
 # load pages scripts
 from app_pages.page_summary import page_summary_body
+from app_pages.page_project_hypothesis import page_project_hypothesis_body
 
 app = MultiPage(app_name= "Heritage") # Create an instance of the app 
 
