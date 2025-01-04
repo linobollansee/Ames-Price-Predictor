905ffd807bdf768674457b4fc9a07a88f6c46381
Lino Bollansee <linobollansee@hotmail.com>
2025-01-04 04:39:35 +0000

    Update app_name in app.py to Heritage Housing Issues

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from app_pages.multipage import MultiPage
 from app_pages.page_summary import page_summary_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
 
-app = MultiPage(app_name= "Heritage") # Create an instance of the app 
+app = MultiPage(app_name= "Heritage Housing Issues") # Create an instance of the app 
 
 # Add your app pages here using .add_page()
 app.add_page("Quick Project Summary", page_summary_body)
