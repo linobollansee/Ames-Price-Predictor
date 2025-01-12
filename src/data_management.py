c3e5f9bf03b7713969fa2531068be7ab7ff05d30
Lino Bollansee <linobollansee@hotmail.com>
2025-01-12 19:41:37 +0000

    Add 2 load data functions to src/data_management.py

--- src/data_management.py
+++ src/data_management.py
@@ -4,10 +4,16 @@ import numpy as np
 import joblib
 
 @st.cache_data
-def load_telco_data():
-    df = pd.read_csv("outputs/datasets/collection/TelcoCustomerChurn.csv")
+def load_house_data():
+    df = pd.read_csv("outputs/datasets/collection/HousePricesRecords.csv")
     return df
 
+
+@st.cache_data
+def load_inherited_data():
+    df = pd.read_csv("outputs/datasets/collection/InheritedHouses.csv")
+    return df_inherited
+    
 
 def load_pkl_file(file_path):
     return joblib.load(filename=file_path)
