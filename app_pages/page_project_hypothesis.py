b763aaeb817f268efd06abf843b6c40b9dd60400
Lino Bollansee <linobollansee@hotmail.com>
2025-01-22 00:24:37 +0000

    app_pages/page_project_hypothesis.py text

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -5,7 +5,18 @@ def page_project_hypothesis_body():
 
     st.write("### Project Hypothesis and Validation")
 
-    # conclusions taken from "" notebook
+    # conclusions taken from "02 - Churned Customer Study" notebook
     st.success(
-        f"* "
-    )
+        f"* We hypothesize that houses with larger square footage tend to "
+        f"have a higher value: Correct. "
+        f"This is supported by the correlation study in the Housing Prices "
+        f"Study. \n\n"
+        f"* We hypothesize that houses in better overall condition tend to "
+        f"have a higher value: Correct. "
+        f"The correlation study in the Housing Prices Study confirms this. "
+        f"\n\n"
+        f"* We hypothesize that houses with a more recent remodel tend to "
+        f"have a higher value: Correct. "
+        f"This is also supported by the correlation study in the Housing "
+        f"Prices Study. \n\n"
+        )
