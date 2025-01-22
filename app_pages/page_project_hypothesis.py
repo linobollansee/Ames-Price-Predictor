a2b76175b8f4824c72afacc0ef8a05bb2bfa52df
Lino Bollansee <linobollansee@hotmail.com>
2025-01-22 00:46:15 +0000

    Add check marks to hypothesis page

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -8,15 +8,15 @@ def page_project_hypothesis_body():
     # conclusions taken from "02 - Churned Customer Study" notebook
     st.success(
         f"* We hypothesize that houses with larger square footage tend to "
-        f"have a higher value: Correct. "
-        f"This is supported by the correlation study in the Housing Prices "
+        f"have a higher value: Correct. \n\n"
+        f"✅ This is supported by the correlation study in the Housing Prices "
         f"Study. \n\n"
         f"* We hypothesize that houses in better overall condition tend to "
-        f"have a higher value: Correct. "
-        f"The correlation study in the Housing Prices Study confirms this. "
+        f"have a higher value: Correct. \n\n"
+        f"✅ The correlation study in the Housing Prices Study confirms this. "
         f"\n\n"
         f"* We hypothesize that houses with a more recent remodel tend to "
-        f"have a higher value: Correct. "
-        f"This is also supported by the correlation study in the Housing "
+        f"have a higher value: Correct. \n\n"
+        f"✅ This is also supported by the correlation study in the Housing "
         f"Prices Study. \n\n"
         )
